fc682055788e9ab7d525a81ce46862b79d4d76f4
mirror-kt <me@mirror-kt.dev>
2020-04-09 00:47:46 +0900

    to relative

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ cd "$GITHUB_WORKSPACE"
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-ktlint --reporter=checkstyle \
+ktlint --relative --reporter=checkstyle \
   | reviewdog -f=checkstyle -name="ktlint" -reporter="${INPUT_REPORTER}" -level="${INPUT_LEVEL}"
